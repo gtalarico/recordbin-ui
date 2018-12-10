e9d906812b9adb4197439e81a4135c3b1fdb391e
gtalarico <gtalarico@gmail.com>
2018-12-10 16:48:51 -0500

    Clear server if login fails

--- src/store/api.store.js
+++ src/store/api.store.js
@@ -13,34 +13,40 @@ export default {
     }
   },
   mutations: {
-    setUserToken(state, value) {
-      state.userToken = value
-    },
     setServerUrl(state, value) {
       state.serverUrl = value
+    },
+    clearServerUrl(state) {
+      state.serverUrl = null
+    },
+    setUserToken(state, value) {
+      state.userToken = value
     },
     clearUserToken(state) {
       state.userToken = null
     },
-    clearUser(state) {
-      state.user = null
-    },
     setUser(state, value) {
       state.user = value
+    },
+    clearUser(state) {
+      state.user = null
     }
   },
   actions: {
     login(context, form) {
-      context.commit("setServerUrl", form.serverUrl)
       return backend
         .login(form)
         .then(userToken => {
+          context.commit("setServerUrl", form.serverUrl)
           context.commit("setUserToken", userToken)
           backend.getUser().then(user => {
             context.commit("setUser", user)
           })
         })
         .catch(error => {
+          context.commit("clearServerUrl")
+          context.commit("clearUserToken")
+          context.commit("clearUser")
           throw error
         })
     },
