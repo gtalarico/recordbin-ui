e8e2abb560b563909bfd9ff69b54005145391929
gtalarico <gtalarico@gmail.com>
2018-12-11 14:28:15 -0500

    Added README to www.recordbin repo

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,6 +5,7 @@ mkdir temp
 cp -r ./dist/ ./temp
 cd temp
 echo "www.recordbin.co" > CNAME
+echo "See [recordbin-ui](https://github.com/RecordBinCo/recordbin-ui)" > README.md
 git init
 git add .
 git commit -m "$commit" --allow-empty
